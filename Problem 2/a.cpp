70504039c19bde138640bda430de7fe687d58d57
Nazifa Tabassum <132453499+tabassumnazifa@users.noreply.github.com>
2025-06-30 08:44:01 +0600

    a.cpp

--- Problem 2/a.cpp
+++ Problem 2/a.cpp
@@ -4,9 +4,9 @@ public:
     bool isPowerOfTwo(int n)
     {
         bool pos=false;
-        for(int i=0; i<n; i++)
+        for(int i=0; i<31; i++)
         {
-            if(1>>i==n)
+            if(1<<i==n)
             {
                 pos=true;
                 break;
