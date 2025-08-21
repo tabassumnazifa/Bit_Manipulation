011bc4ac3d03e86a6fa8619bf01303ae630a2e77
Nazifa Tabassum <132453499+tabassumnazifa@users.noreply.github.com>
2025-08-21 14:31:41 +0600

    Add files via upload

---
+++ Problem 6/nazifa.cpp
@@ -0,0 +1,14 @@
+class Solution
+{
+public:
+    int getSum(int a, int b)
+    {
+        while(b!=0)
+        {
+            int car=a&b;
+            a=a^b;
+            b=car<<1;
+        }
+        return a;
+    }
+};
